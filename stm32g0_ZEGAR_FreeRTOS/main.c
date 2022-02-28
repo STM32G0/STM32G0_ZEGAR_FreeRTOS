5391a97ba98767f4a792bbc473b7a441dce415a4
STM32G0 <stm32g0@gmail.com>
2022-02-28 14:44:49 +0100

    taskdisplay

--- stm32g0_ZEGAR_FreeRTOS/main.c
+++ stm32g0_ZEGAR_FreeRTOS/main.c
@@ -18,7 +18,7 @@ IDE   : SEGGER Embedded Studio
 #include "task.h"
 #include "queue.h"
 #include "semphr.h"
-//#define debug
+#define debug
 
 
 static void prvSetupHardware(void);
@@ -36,8 +36,8 @@ int main(void) {
 	prvSetupHardware();
         
        	// Creating tasks
-	 assert( xTaskCreate( vClockTask, "ClockTask", 128, NULL, 4 , NULL) == pdPASS); // assert create task control
-         assert( xTaskCreate( vDisplayTask, "DisplayTask", 128, NULL, 4 , NULL) == pdPASS); // assert create task control
+	 assert( xTaskCreate( vClockTask, "ClockTask", 256, NULL, 4 , NULL) == pdPASS); // assert create task control
+         assert( xTaskCreate( vDisplayTask, "DisplayTask", 256, NULL, 3 , NULL) == pdPASS); // assert create task control
         
 	// Start the scheduler
 	vTaskStartScheduler(); // should never return
@@ -62,23 +62,24 @@ static void prvSetupHardware(void)
 
 void vClockTask(void *pvParameters) {
 
-struct mcp79410_time {
+struct {
 uint8_t HOUR;
 uint8_t MIN; 
 uint8_t SEC;
-} mcp79410_time={0,0,0} ;
+} MCP79410_Time_ClockTask={0,0,0} ;
 
 /* Attempt to create a queue. */
-xQueueClockTask = xQueueCreate( 2, sizeof( mcp79410_time ) );
-assert(xQueueClockTask != NULL); // assert create queue control
+    xQueueClockTask = xQueueCreate( 2, sizeof( MCP79410_Time_ClockTask ) );
+    assert(xQueueClockTask != NULL); // assert create queue control
 
 /* Attempt to create a semaphore. */
     xSemaphoreClockTask = xSemaphoreCreateBinary();
     assert(xSemaphoreClockTask != NULL); // assert create semaphore control
+    xSemaphoreGive( xSemaphoreClockTask ); //load the semaphore with a token
     
 	for (;;) {
 	
-        if( xSemaphoreTake( xSemaphoreClockTask, portMAX_DELAY == pdTRUE ))
+        if( xSemaphoreTake( xSemaphoreClockTask, portMAX_DELAY) == pdTRUE )
         {
                       
             #ifdef debug   
@@ -86,11 +87,13 @@ assert(xQueueClockTask != NULL); // assert create queue control
             #endif
             
             LED2_Toggle();
-            mcp79410_time.SEC = mcp79410.getTime_SEC(); 
-            mcp79410_time.MIN = mcp79410.getTime_MIN();   
-            mcp79410_time.HOUR = mcp79410.getTime_HOUR();
+            /* sekcja krytyczna start ??? */
+            MCP79410_Time_ClockTask.SEC = mcp79410.getTime_SEC(); 
+            MCP79410_Time_ClockTask.MIN = mcp79410.getTime_MIN();   
+            MCP79410_Time_ClockTask.HOUR = mcp79410.getTime_HOUR();
+            /* sekcja krytyczna end ??? */
             if( xQueueClockTask != NULL ){
-            xQueueSend( xQueueClockTask, ( void * ) &mcp79410_time, ( TickType_t ) 0 );
+            xQueueSend( xQueueClockTask, ( void * ) &MCP79410_Time_ClockTask, ( TickType_t ) 0 );
             }
                                                    
         }
@@ -102,28 +105,25 @@ assert(xQueueClockTask != NULL); // assert create queue control
 
 void vDisplayTask(void *pvParameters) {
 
-struct mcp79410_time {
+struct {
 uint8_t HOUR;
 uint8_t MIN; 
 uint8_t SEC;
-} mcp79410_time={0,0,0} ;
+} MCP79410_Time_DisplayTask={0,0,0} ;
 
     
 	for (;;) {
-	
-                        
-            #ifdef debug   
-            printf("Hello vDisplayTask\n"); 
-            #endif
-            
+	    
            LED1_Toggle();
          
-           if(xQueueReceive( xQueueClockTask, & mcp79410_time, ( TickType_t ) 10 ) == pdPASS ){
-                /* mcp79410_time now contains a copy of xMessage. */
-                              
+           if(xQueueReceive( xQueueClockTask, & MCP79410_Time_DisplayTask, portMAX_DELAY) == pdPASS ){
+                /* mcp79410_time now contains a copy of MCP79410_Time_DisplayTask  . */
+                #ifdef debug   
+                printf("Hello vDisplayTask\n"); 
+                #endif             
                 /* display minutes*/
-                max7219.SendToDevice(Device0, MAX7219_DIGIT0, (mcp79410_time.MIN & 0x0F)) ;       // wyswietl cyfre jednosci
-                max7219.SendToDevice(Device0, MAX7219_DIGIT1, ((mcp79410_time.MIN >> 4)& 0x0F)) ; // wyswietl cyfre dziesiatki
+                max7219.SendToDevice(Device0, MAX7219_DIGIT0, (MCP79410_Time_DisplayTask.MIN & 0x0F)) ;       // wyswietl cyfre jednosci
+                max7219.SendToDevice(Device0, MAX7219_DIGIT1, ((MCP79410_Time_DisplayTask.MIN >> 4)& 0x0F)) ; // wyswietl cyfre dziesiatki
       }
    }                  
       
