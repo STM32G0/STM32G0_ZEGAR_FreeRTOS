ed9ae5b59569afbfb5bb75ba25e6d90848247ffe
STM32G0 <stm32g0@gmail.com>
2022-03-07 08:51:48 +0100

    max7219 init

--- stm32g0_ZEGAR_FreeRTOS/max7219.c
+++ stm32g0_ZEGAR_FreeRTOS/max7219.c
@@ -15,55 +15,52 @@ uint8_t Max7219SpiBuffer[MAX7219_DEVICES * 2];
 // wyslanie 2 bajtów do ukladu MAX7219
 // 1 bajt (adress) - adres rejestru do którego bêd¹ wpisane dane
 // 2 bajt (data) - dane do zapisania
-void max7219_send(uint8_t adress, uint8_t data)
-{
-    CS_SetLow();	// ustawienie stanu niskiego na linii CS
-    SPI1_Transmit_2x8bit(adress, data);
-    CS_SetHigh();	// ustawienie stanu wysokiego na linii CS (zatrzaœniêcie danych w ukladzie zboczem narastajacym)
+void max7219_send(uint8_t adress, uint8_t data) {
+  CS_SetLow(); // ustawienie stanu niskiego na linii CS
+  SPI1_Transmit_2x8bit(adress, data);
+  CS_SetHigh(); // ustawienie stanu wysokiego na linii CS (zatrzaœniêcie danych w ukladzie zboczem narastajacym)
+}
+
+void max7219_SendToDevice(uint8_t DeviceNumber, uint8_t Adress, uint8_t Data) {
+  uint8_t Offset = DeviceNumber * 2;
+/* Device 0 - send NOP (0,0) and next send (adresRegister , data) */
+/* Device 1 - send (adresRegister , data) and next send NOP (0,0) */
+
+  memset(Max7219SpiBuffer, 0x00, (MAX7219_DEVICES * 2)); // set NOP
+  Max7219SpiBuffer[(MAX7219_DEVICES * 2) - Offset - 2] = Adress;
+  Max7219SpiBuffer[(MAX7219_DEVICES * 2) - Offset - 1] = Data;
+  CS_SetLow(); // ustawienie stanu niskiego na linii CS
+  SPI1_Transmit_Buffer(Max7219SpiBuffer, (MAX7219_DEVICES * 2));
+  CS_SetHigh(); // ustawienie stanu wysokiego na linii CS (zatrzaœniêcie danych w ukladzie zboczem narastajacym)
 }
 
 // inicjalizacja ukladu MAX7219
-void max7219_init(void)
-{
-	max7219_send(MAX7219_SCAN_LIMIT, (MAX7219_USE_DIGIT-1));  // liczba obslugiwanych wyswietlaczy
-	max7219_send(MAX7219_INTENSITY, 15);	// maksymalna jasnosc od zera 0 - 15
-	max7219_send(MAX7219_DECODE_MODE, MAX7219_USE_BCD_DECODE); // ustawienie trybu wyswietlania dla poszczególnych wyswietlaczy - BCD
-	max7219_send(MAX7219_SHUTDOWN, MAX7219_NORMAL_OPPERATION);	// wlaczenie ukladu (normal operation)
-        max7219_send(MAX7219_DISPLAY_TEST, 0) ; //Test OFF
-}
-
-
-
-void max7219_SendToDevice(uint8_t DeviceNumber, uint8_t Adress, uint8_t Data)
-{
-	uint8_t Offset = DeviceNumber * 2;
-
-	memset(Max7219SpiBuffer, 0x00, (MAX7219_DEVICES * 2)); // set NOP
-	Max7219SpiBuffer[(MAX7219_DEVICES * 2)-Offset-2] = Adress;
-	Max7219SpiBuffer[(MAX7219_DEVICES * 2)-Offset-1] = Data;
-        CS_SetLow();	// ustawienie stanu niskiego na linii CS
-        SPI1_Transmit_Buffer( Max7219SpiBuffer, (MAX7219_DEVICES * 2));
-	CS_SetHigh();	// ustawienie stanu wysokiego na linii CS (zatrzaœniêcie danych w ukladzie zboczem narastajacym)
-	
+void max7219_init(void) {
+  /* Device 1*/
+  max7219_SendToDevice(Device0, MAX7219_SCAN_LIMIT, (MAX7219_USE_DIGIT - 1)); // liczba obslugiwanych wyswietlaczy
+  max7219_SendToDevice(Device0, MAX7219_INTENSITY, 15);                       // maksymalna jasnosc od zera 0 - 15
+  max7219_SendToDevice(Device0, MAX7219_DECODE_MODE, MAX7219_USE_BCD_DECODE); // ustawienie trybu wyswietlania dla poszczeglnych wyswietlaczy - BCD
+  max7219_SendToDevice(Device0, MAX7219_SHUTDOWN, MAX7219_NORMAL_OPPERATION); // wlaczenie ukladu (normal operation)
+  max7219_SendToDevice(Device0, MAX7219_DISPLAY_TEST, 0);                     // Test OFF
+  /* Device 1*/
+  max7219_SendToDevice(Device1, MAX7219_SCAN_LIMIT, (MAX7219_USE_DIGIT - 1)); // liczba obslugiwanych wyswietlaczy
+  max7219_SendToDevice(Device1, MAX7219_INTENSITY, 15);                       // maksymalna jasnosc od zera 0 - 15
+  max7219_SendToDevice(Device1, MAX7219_DECODE_MODE, MAX7219_USE_BCD_DECODE); // ustawienie trybu wyswietlania dla poszczeglnych wyswietlaczy - BCD
+  max7219_SendToDevice(Device1, MAX7219_SHUTDOWN, MAX7219_NORMAL_OPPERATION); // wlaczenie ukladu (normal operation)
+  max7219_SendToDevice(Device1, MAX7219_DISPLAY_TEST, 0);                     // Test OFF
 }
 
 // czyszczenie zawartosci wszystkich wyswietlaczy LED
 
-void max7219_clear(void)
-{
-	uint8_t i, j;
-	for(i = 0; i < MAX7219_DEVICES; i++)
-	{
-		for(j = 0; j < 8; j++)
-		{
-                if ((MAX7219_USE_BCD_DECODE & (1<<i))){
-                          max7219_SendToDevice(i, j,0xF);
-                        }
-                        else {
-                          max7219_SendToDevice(i, j,0);
-                        }
-
-		}
-	}
-	
+void max7219_clear(void) {
+  uint8_t i, j;
+  for (i = 0; i < MAX7219_DEVICES; i++) {
+    for (j = 0; j < 8; j++) {
+      if ((MAX7219_USE_BCD_DECODE & (1 << i))) {
+        max7219_SendToDevice(i, j, 0xF);
+      } else {
+        max7219_SendToDevice(i, j, 0);
+      }
+    }
+  }
 }
