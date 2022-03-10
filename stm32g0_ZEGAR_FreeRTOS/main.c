54a549936e5efe1c6fb50142e62f5becdbaa258f
STM32G0 <stm32g0@gmail.com>
2022-03-10 14:57:48 +0100

    konfiguracja dotyku

--- stm32g0_ZEGAR_FreeRTOS/main.c
+++ stm32g0_ZEGAR_FreeRTOS/main.c
@@ -31,7 +31,7 @@ volatile uint8_t kropka_int_flag = 0 ;
 
 
 int main(void) {
-  /* FreeRTOS APP support create */
+  /* FreeRTOS APP support */
   TaskCreate();
   SemaphoreCreate();
   QueueCreate();
@@ -80,8 +80,8 @@ void vClockTask(void *pvParameters) {
       MCP79410_Time_ClockTask.MIN = mcp79410.getTime_MIN();
       MCP79410_Time_ClockTask.HOUR = mcp79410.getTime_HOUR();
       /* sekcja krytyczna end ??? */
-      if (xQueueClockTask != NULL) {
-        xQueueSend(xQueueClockTask, (void *)&MCP79410_Time_ClockTask, (TickType_t)0);
+      if (xQueueClockTask != NULL) {// wysy³amy do kolejki dane (w strukturze) o czasie, pobrane z MCP79410, 
+        xQueueSend(xQueueClockTask, (void *)&MCP79410_Time_ClockTask, (TickType_t)0); //Timeout = 0 - bez blokowania zadania
       }
     }
   }
@@ -90,64 +90,69 @@ void vClockTask(void *pvParameters) {
 void vDisplayTask(void *pvParameters) {
 
   time_t MCP79410_Time_DisplayTask = {0, 0, 0};
+  uint8_t minute_segment_mask = 1;
+  uint8_t hour_segment_mask = 2;
 
   for (;;) {
 
-    if (xQueueReceive(xQueueClockTask, &MCP79410_Time_DisplayTask, portMAX_DELAY) == pdPASS) {
+    if (xQueueReceive(xQueueClockTask, &MCP79410_Time_DisplayTask, portMAX_DELAY) == pdPASS) {// pobierz dane z kolejki jeœli s¹ dostêpne, zapisz je do struktury MCP79410_Time_DisplayTask
 /* mcp79410_time now contains a copy of MCP79410_Time_DisplayTask  . */
 #ifdef debug
       printf("Hello vDisplayTask\n");
 #endif
-       uint32_t notificationvalue = ulTaskNotifyTake( pdTRUE, 0 );
+      uint32_t notificationvalue = ulTaskNotifyTake(pdTRUE, 0); // Timeout = 0 - bez blokowania zadania
+
       /* display minutes*/
 
       max7219.SendToDevice(Device0, MAX7219_DIGIT0, (MCP79410_Time_DisplayTask.MIN & 0x0F));        // wyswietl cyfre jednosci
       max7219.SendToDevice(Device0, MAX7219_DIGIT1, ((MCP79410_Time_DisplayTask.MIN >> 4) & 0x0F)); // wyswietl cyfre dziesiatki
-     
-      if(notificationvalue & 0x02) {
+
+      if (notificationvalue & minute_segment_mask) { // migniêcie segmentem minut dla potrzeb sygnalizacji ustawiania czasu
         // wygaœ segmenty dla minut
         max7219.SendToDevice(Device0, MAX7219_DIGIT0, 0xF); // zgaœ cyfrê jednosci
         max7219.SendToDevice(Device0, MAX7219_DIGIT1, 0xF); // zgaœ cyfrê dziesiatki
-        
       }
 
       /* display hour*/
 
       max7219.SendToDevice(Device0, MAX7219_DIGIT2, (MCP79410_Time_DisplayTask.HOUR & 0x0F) | kropka_int_flag); // wyswietl cyfre jednosci plus kropka
       max7219.SendToDevice(Device0, MAX7219_DIGIT3, ((MCP79410_Time_DisplayTask.HOUR >> 4) & 0x0F));            // wyswietl cyfre dziesiatki
-      
-     // if (touch_SELECT_flag == 2) {
-        // wygaœ segmenty dla minut
-     //   max7219.SendToDevice(Device0, MAX7219_DIGIT2, 0xF); // zgaœ cyfrê jednosci
-      //  max7219.SendToDevice(Device0, MAX7219_DIGIT3, 0xF); // zgaœ cyfrê dziesiatki
-        
-     // }
+
+      if (notificationvalue & hour_segment_mask) { // migniêcie segmentem godzin dla potrzeb sygnalizacji ustawiania czasu
+        // wygaœ segmenty dla godzin
+        max7219.SendToDevice(Device0, MAX7219_DIGIT2, 0xF); // zgaœ cyfrê jednosci
+        max7219.SendToDevice(Device0, MAX7219_DIGIT3, 0xF); // zgaœ cyfrê dziesiatki
+      }
     }
   }
 }
 
 void vTouchTask(void *pvParameters) {
-static uint8_t touch_SELECT_flag = 0;
+
+static uint8_t touch_SELECT_counter = 0;
+  
   for (;;) {
 
     if (touch_int_flag) {
-      touch_int_flag = 0;                                                 // zeruj flagê dotyku
-      if (cap1293.ReadRegister(CAP1293_SENSTATUS) & CAP1293_CS3_SELECT) { // dotyk pola CS3 "SELECT"
-        xTaskNotify(xDisplayTaskHandle, ( 1UL << 1UL ), eSetBits );
-        //touch_SELECT_flag++;                                              // ustaw flagê dla dotyku pola SELECT
-       // if (touch_SELECT_flag > 2){
-       //   touch_SELECT_flag = 0;
-       //   }
+        touch_int_flag = 0;   // zeruj flagê dotyku otrzyman¹ z przerwania EXTI4_15_IRQHandler
+      
+      if (cap1293.ReadRegister(CAP1293_SENSTATUS) & CAP1293_CS3_SELECT) { // dotyk pola CS3 "SELECT" ?
+        xTaskNotify(xDisplayTaskHandle, ( 1 << touch_SELECT_counter++ ), eSetBits ); //wyœlij do xDisplayTask flagê, bit 1 lub bit 2 ustawiony
+                                              
+        if (touch_SELECT_counter > 1){
+          touch_SELECT_counter = 0;
+          }
       }
 
       cap1293.WriteRegister(CAP1293_MAIN, (cap1293.ReadRegister(CAP1293_MAIN) & ~CAP1293_MAIN_INT)); // clear main interrupt
-
+      
 #ifdef debug
       printf("Hello vTouchTask\n");
 #endif
     }
   }
 }
+
 
 /*signal on PB5 pin generate IRQ*/
 void EXTI4_15_IRQHandler(void) {
@@ -168,9 +173,8 @@ void EXTI4_15_IRQHandler(void) {
   /* Inerrupt from CAP1293 ALERT - PB4 ? */
 
   if (EXTI->FPR1 & EXTI_FPR1_FPIF4) {
-    EXTI->FPR1 |= EXTI_FPR1_FPIF4; // clear pending
-    touch_int_flag = 1 ;
-        
+    touch_int_flag = 1 ; //flaga wykorzystana w zadaniu vTouchTask
+    EXTI->FPR1 |= EXTI_FPR1_FPIF4; // clear pending    
   }
 }
 
@@ -178,6 +182,3 @@ void EXTI4_15_IRQHandler(void) {
 
 
 
-
-// if(cap1293.read(CAP1293_SENSTATUS) == 1) GPIOA->ODR |= GPIO_ODR_OD8 ;  //dotyk pierwszego pola wykryty LED ON
-// if(cap1293.read(CAP1293_SENSTATUS) == 2) GPIOA->ODR &= ~GPIO_ODR_OD8 ; //dotyk drugiego pola wykryty LED OFF
